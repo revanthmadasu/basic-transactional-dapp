a035ed498ba60dbba8195562475490de39b75b1a
Revanth Madasu <revanthmadasu913@gmail.com>
2022-01-24 21:10:14 +0530

    form handling

--- src/components/TransactionTab.jsx
+++ src/components/TransactionTab.jsx
@@ -1,27 +1,50 @@
+import { useState } from "react";
+
 export const TransactionTab = () => {
+    const [form, setForm] = useState({
+        senderEth: "",
+        receiverEth: "",
+        amount: "",
+        disabled: true
+    });
+
+    const inputChangeHandler = (field, event) => {
+        const newVals = {
+            ...form,
+            [field]: event.target.value
+        };
+        setForm({
+            ...newVals,
+            disabled: !isFormValid(newVals)
+        });
+    };
+    const isFormValid = (form) => (form.senderEth && form.receiverEth && form.amount);
     return (
         <div className="content form text-left p-1">
             <h4>Transfer <span className="gold-color">Ethereum</span></h4>
             <form action="">
                 <div className="form-group d-flex flex-column my-2">
                     <label htmlFor="senderAddress">Sender ETH Address</label>
-                    <input type="text" className="formControl" id="senderAddress" placeholder="Enter your address" />
+                    <input type="text" className="formControl" id="senderAddress" placeholder="Enter your address"
+                        value={form.senderEth} onChange={(event) => inputChangeHandler("senderEth", event)} />
                     <small className="form-text text-muted">Enter your wallet address. Note: you will need to approve this with your private key.</small>
                 </div>
                 <div className="form-group d-flex flex-column my-2">
                     <label htmlFor="receiverAddress">Receiver ETH Address</label>
-                    <input type="text" className="formControl" id="receiverAddress" placeholder="Enter receiver address" />
+                    <input type="text" className="formControl" id="receiverAddress" placeholder="Enter receiver address"
+                        value={form.receiverEth} onChange={(event) => inputChangeHandler("receiverEth", event)}  />
                     <small className="form-text text-muted">Enter the wallet address of the recipient.</small>
                 </div>
                 <div className="form-group d-flex flex-column my-2">
                     <label htmlFor="amount">Amount</label>
-                    <input type="number" className="formControl focus-gold-outline" id="amount" placeholder="Enter amount" />
+                    <input type="number" className="formControl focus-gold-outline" id="amount" placeholder="Enter amount" 
+                        value={form.amount} onChange={(event) => inputChangeHandler("amount", event)} />
                     <small className="form-text text-muted">How much you want to send in ETH.</small>
                 </div>
 
                 <div className="actions text-right">
                     <div className="d-inline-flex flex-column my-2">
-                        <button type="submit" className="btn btn-primary">Transfer</button>
+                        <button type="submit" className="btn btn-primary" disabled={form.disabled}>Transfer</button>
                         <div>
                             <small className="form-text text-muted">Click the button to deposit your ETH to the contract.</small>
                         </div>
